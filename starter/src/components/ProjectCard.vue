7aa8453b7259e16005771ec74227b0e3e20eacb0
Wei199a <1522129824@qq.com>
2026-09-14 16:16:24 +0800

    Add P1 lesson 06 PPT-guided student resources v1.1

---
+++ starter/src/components/ProjectCard.vue
@@ -0,0 +1,9 @@
+<script setup>
+// TODO-L6: 声明 project 输入和 select 事件。
+</script>
+
+<template>
+  <article class="project-card">
+    <p>请完成本课的作品卡片组件</p>
+  </article>
+</template>
---
+++ starter/src/components/SiteFooter.vue
@@ -0,0 +1,3 @@
+<template>
+  <footer><small>&copy; 2026 林晓 · P1 v2.0</small></footer>
+</template>
